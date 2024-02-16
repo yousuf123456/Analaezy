5393f6ac209486e8d2eb0bf726dc6e54359216b0
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-02-16 17:10:12 +0500

    appDir config Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    appDir: true,
     serverActions: true,
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
