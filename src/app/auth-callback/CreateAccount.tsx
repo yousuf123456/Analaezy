87b7ef8bd3fe8d0b2dacc0ac83550e410a9eaa07
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-02-16 16:51:14 +0500

    Prerender error on auth-callback page fix

---
+++ src/app/auth-callback/CreateAccount.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+
+import { Loader2 } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { trpc } from "../_trpc/client";
+
+export const CreateAccount = () => {
+  const router = useRouter();
+  const origin = useSearchParams().get("origin");
+
+  trpc.authCallback.useQuery(undefined, {
+    onSuccess: (data) => {
+      if (data.success) {
+        return origin ? router.push(`/${origin}`) : router.push("dashboard");
+      }
+    },
+
+    onError: (err) => {
+      if (err.data?.code === "UNAUTHORIZED") {
+        return router.push("/sign-in");
+      }
+      console.log(err);
+    },
+
+    retry: true,
+    retryDelay: 500,
+  });
+
+  return (
+    <div className="mt-36 w-full flex gap-5 justify-center items-center">
+      <Loader2 className="animate-spin text-zinc-700 w-8 h-8" />
+      <div className="flex flex-col">
+        <p className="text-xl font-semibold text-zinc-600">
+          Creating your account.
+        </p>
+        <p className=" text-zinc-600">You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
+};
