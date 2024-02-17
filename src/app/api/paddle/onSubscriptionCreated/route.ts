946595bbf17ba3123f788ff19dc494b073bd4efe
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-02-17 14:20:52 +0500

    Webhooks Testing

--- src/app/api/paddle/onSubscriptionCreated/route.ts
+++ src/app/api/paddle/onSubscriptionCreated/route.ts
@@ -3,8 +3,18 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
-    const body = await req.json();
-    console.log(body);
+    const { data } = await req.json();
+    const subscriptionId = data.id;
+
+    const response = await fetch(
+      `https://api.paddle.com/subscriptions/${subscriptionId}`,
+      {
+        method: "GET",
+      }
+    );
+
+    const { data: subData } = await response.json();
+    console.log(data.management_urls);
 
     return NextResponse.json("Async user data with subscription succesfully");
   } catch (e) {
